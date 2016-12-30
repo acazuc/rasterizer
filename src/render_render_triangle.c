a46402a9437e6bf3f0475fbfee53e1e0605597a3
Jukino <acazuc@student.42.fr>
2016-12-30 18:53:10 +0100

    Some changes

--- src/render_render_triangle.c
+++ src/render_render_triangle.c
@@ -6,12 +6,12 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/30 12:02:18 by acazuc            #+#    #+#             */
-/*   Updated: 2016/12/30 17:56:57 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/12/30 18:42:40 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rasterizer.h"
-
+/*
 static void		_render_span(t_render *render, t_triangle_span *span, double y)
 {
 	t_color	color;
@@ -77,20 +77,46 @@ static void		_render_span_edges(t_render *render, t_triangle_edge *e1
 	}
 }
 
+static void		_build_edges(t_triangle_edge *edges, t_triangle *triangle)
+{
+	edges[0].c1 = triangle->v1.color;
+	edges[0].x1 = triangle->v1.x;
+	edges[0].y1 = triangle->v1.y;
+	edges[0].z1 = triangle->v1.z;
+	edges[0].c2 = triangle->v2.color;
+	edges[0].x2 = triangle->v2.x;
+	edges[0].y2 = triangle->v2.y;
+	edges[0].z2 = triangle->v2.z;
+	edges[1].c1 = triangle->v2.color;
+	edges[1].x1 = triangle->v2.x;
+	edges[1].y1 = triangle->v2.y;
+	edges[1].z1 = triangle->v2.z;
+	edges[1].c2 = triangle->v3.color;
+	edges[1].x2 = triangle->v3.x;
+	edges[1].y2 = triangle->v3.y;
+	edges[1].z2 = triangle->v3.z;
+	edges[2].c1 = triangle->v3.color;
+	edges[2].x1 = triangle->v3.x;
+	edges[2].y1 = triangle->v3.y;
+	edges[2].z1 = triangle->v3.z;
+	edges[2].c2 = triangle->v1.color;
+	edges[2].x2 = triangle->v1.x;
+	edges[2].y2 = triangle->v1.y;
+	edges[2].z2 = triangle->v1.z;
+}*/
+
 void			render_render_triangle(t_render *render, t_triangle *triangle)
 {
-	t_triangle_edge		edges[3] = {{triangle->v1.color, triangle->v1.x
-	, triangle->v1.y, triangle->v1.z, triangle->v2.color, triangle->v2.x
-	, triangle->v2.y, triangle->v2.z}, {triangle->v2.color, triangle->v2.x
-	, triangle->v2.y, triangle->v2.z, triangle->v3.color, triangle->v3.x
-	, triangle->v3.y, triangle->v3.z}, {triangle->v3.color, triangle->v3.x
-	, triangle->v3.y, triangle->v3.z, triangle->v1.color, triangle->v1.x
-	, triangle->v1.y, triangle->v1.z}};
+	render_render_line(render, &triangle->v1, &triangle->v2);
+	render_render_line(render, &triangle->v2, &triangle->v3);
+	render_render_line(render, &triangle->v3, &triangle->v1);
+	/*t_triangle_edge			edges[3];
 	double				max_length;
 	int					long_edge;
 	double				length;
 	int					i;
 
+	_build_edges(edges, triangle);
 	i = 0;
 	length = 0;
 	long_edge = 0;
@@ -110,5 +136,5 @@ void			render_render_triangle(t_render *render, t_triangle *triangle)
 		++i;
 	}
 	_render_span_edges(render, &edges[long_edge], &edges[(long_edge + 1) % 3]);
-	_render_span_edges(render, &edges[long_edge], &edges[(long_edge + 2) % 3]);
+	_render_span_edges(render, &edges[long_edge], &edges[(long_edge + 2) % 3]);*/
 }
